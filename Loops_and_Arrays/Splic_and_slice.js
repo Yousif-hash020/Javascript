cc87fb81e69f27e217a99d4e8e5aa5f74e681cc5
Yousif-hash020 <arainyousif020@gmail.com>
2026-07-30 23:59:23 +0500

    Splice and Slice

---
+++ Loops_and_Arrays/Splic_and_slice.js
@@ -0,0 +1,11 @@
+arr = [1,2,3,4,5]
+
+r_arr = arr.splice(2,1,4)
+
+console.log(arr)
+console.log(r_arr,'this is splice method')
+
+slice_arr = arr.slice(0,2)
+
+console.log(slice_arr,'this is slice method ')
+console.log(arr)
