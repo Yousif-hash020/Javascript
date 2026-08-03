6f4208abb2522586ba06f9963f199c73ad188a42
Yousif-hash020 <arainyousif020@gmail.com>
2026-08-03 18:42:27 +0500

    DOM

---
+++ DOM/Dom_manipulation.js
@@ -0,0 +1,3 @@
+let h1 = document.createElement("h1");
+h1.textContent = "This is a heading";
+document.querySelector("body").prepend(h1);
---
+++ DOM/attribute.js
@@ -0,0 +1,8 @@
+let a = document.querySelector("a");
+a.setAttribute("href", "https://www.google.com")
+
+let b = document.querySelector("a");
+console.log(b.getAttribute("href"))
+
+let c = document.querySelector("a");
+c.removeAttribute("href")
