ea99884c59b59db87636d006624c5b6c0f78333f
Yousif-hash020 <arainyousif020@gmail.com>
2026-08-03 22:30:03 +0500

    FIle_Inp

--- DOM/fileinp.js
+++ DOM/fileinp.js
@@ -4,3 +4,7 @@ let h1 = document.querySelector("h1");
 h1.addEventListener("click", function(){
     fileinp.click();
 })
+
+fileinp.addEventListener("change", function(e){
+    h1.textContent = e.target.files[0].name;
+})
