96ba18ac9f7da29bd09c43275ba7a1e93e8be334
Yousif-hash020 <arainyousif020@gmail.com>
2026-08-03 19:03:08 +0500

    DOM

--- DOM/Dom_manipulation.js
+++ DOM/Dom_manipulation.js
@@ -1,3 +1,7 @@
 let h1 = document.createElement("h1");
 h1.textContent = "This is a heading";
 document.querySelector("body").prepend(h1);
+
+
+let p = document.querySelector("p");
+p.classList.add("hulu")
