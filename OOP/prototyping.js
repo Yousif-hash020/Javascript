b7c0927d092d9a0f8711ad07f134acd4284d7232
Yousif-hash020 <arainyousif020@gmail.com>
2026-08-05 19:04:32 +0500

    Prototype

---
+++ OOP/prototyping.js
@@ -0,0 +1,16 @@
+function CreatePencil(name, price, color, company){
+
+    this.name = name;
+    this.price = price;
+    this.color = color;
+    this.company = company;
+}
+
+CreatePencil.prototype.write = function(text){
+    let h1 = document.createElement("h1");
+    h1.textContent = text;
+    h1.style.color = this.color;
+    document.body.append(h1)
+}
+
+let pencil1 = new CreatePencil("Clipper",20, "blue","Dollar" );
