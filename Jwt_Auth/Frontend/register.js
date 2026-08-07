5c44db74200e0c405d697472dd87405c4b3e1235
Yousif-hash020 <arainyousif020@gmail.com>
2026-08-08 00:38:17 +0500

    Jwt_User_registration

---
+++ Jwt_Auth/Frontend/register.js
@@ -0,0 +1,36 @@
+let register = document.querySelector("#registerform");
+
+register.addEventListener("submit", async (e) => {
+    e.preventDefault()
+    let name = document.querySelector("#name").value;
+    let email = document.querySelector("#email").value;
+    let password = document.querySelector("#password").value;
+
+    try {
+
+        const res = await fetch("http://localhost:5000/api/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name, email, password })
+        });
+
+        const data = await res.json();
+        if (!res.ok) {
+            document.querySelector("#message").textContent = data.message;
+            return
+        }
+
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+        document.querySelector("#message").textContent = data.message;
+        console.log(data);
+        register.reset();
+
+    } catch (err) {
+          document.querySelector("#message").textContent = "something went wrong";
+          console.log(err)
+
+    }
+})
